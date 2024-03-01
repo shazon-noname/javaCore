4c76048acaee925ebc299aa7a300489e794a4bf1
shazon <maryan.20083@gmail.com>
2024-03-01 12:17:09 +0200

    CloneObjects and PracticeCloneObj files add

---
+++ CloneObjects/CloneObjects.java
@@ -0,0 +1,39 @@
+package CloneObjects;
+
+public class CloneObjects {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        MyObject myObject = new MyObject();
+        myObject.i = 1;
+        NewObject newObject = new NewObject();
+        newObject.j = 3;
+        myObject.newObject = newObject;
+        MyObject myObject1 = myObject.clone();
+        myObject1.i = 2;
+        System.out.println("myObject.i = " + myObject.i);
+        System.out.println("myObject1.i = " + myObject1.i);
+        myObject1.newObject.j = 4;
+        System.out.println("myObject.j = " + myObject.newObject.j);
+        System.out.println("myObject1.j = " + myObject1.newObject.j);
+    }
+}
+
+class MyObject implements Cloneable {
+    int i;
+    NewObject newObject;
+
+    @Override
+    protected MyObject clone() throws CloneNotSupportedException {
+        MyObject myObject = new MyObject();
+        myObject.newObject = newObject.clone();
+        return myObject;
+    }
+}
+
+class NewObject implements Cloneable {
+    int j;
+
+    @Override
+    protected NewObject clone() throws CloneNotSupportedException {
+        return (NewObject) super.clone();
+    }
+}
---
+++ CloneObjects/PracticeCloneObj.java
@@ -0,0 +1,68 @@
+package CloneObjects;
+
+class PracticeCloneObj {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        FamilyName familyName = new FamilyName("Marian");
+        FamilyLastName familyLastName = new FamilyLastName("Zinkevych");
+        familyName.setFamilyLastName(familyLastName);
+        System.out.println(familyName.getName() + " " + familyLastName.getLastName());
+        FamilyName familyName1 = familyName.clone();
+        familyName1.setName("David");
+        System.out.println(familyName1.getName() + " " + familyName1.getFamilyLastName().getLastName());
+        familyName1.getFamilyLastName().setLastName("Avetisov");
+        System.out.println(familyName1.getName() + " " + familyName1.getFamilyLastName().getLastName());
+    }
+}
+
+class FamilyName implements Cloneable {
+    private String name;
+    private FamilyLastName familyLastName;
+
+    public FamilyName(String name) {
+        this.name = name;
+    }
+
+    public FamilyLastName getFamilyLastName() {
+        return familyLastName;
+    }
+
+    public void setFamilyLastName(FamilyLastName familyLastName) {
+        this.familyLastName = familyLastName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    protected FamilyName clone() throws CloneNotSupportedException {
+        FamilyName familyName = new FamilyName(name);
+        familyName.familyLastName = familyLastName.clone();
+        return familyName;
+    }
+}
+
+class FamilyLastName implements Cloneable {
+    private String lastName;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public FamilyLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    protected FamilyLastName clone() throws CloneNotSupportedException {
+        return (FamilyLastName) super.clone();
+    }
+}
