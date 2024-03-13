b7e080d9c406b29c1235d361def1d73a509ccafe
shazon <maryan.20083@gmail.com>
2024-03-13 06:32:19 +0200

    Added CountDownLatch, Exchanger, ScheduleExcecuterService, Semafors

---
+++ Multithreading/Exchanger/Main.java
@@ -0,0 +1,52 @@
+package Multithreading.Exchanger;
+
+import java.util.concurrent.Exchanger;
+
+public class Main {
+    public static void main(String[] args) {
+        Exchanger<String> exchanger = new Exchanger<>();
+
+        new Mike(exchanger);
+        new Anket((exchanger));
+    }
+
+    static class Mike extends Thread {
+        Exchanger<String> exchanger;
+
+        public Mike(Exchanger<String> exchanger) {
+            this.exchanger = exchanger;
+            start();
+        }
+
+        @Override
+        public void run() {
+            try {
+                System.out.println(exchanger.exchange(null));
+                exchanger.exchange("I`m 20 years old");
+                sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    static class Anket extends Thread {
+        Exchanger<String> exchanger;
+
+        public Anket(Exchanger<String> exchanger) {
+            this.exchanger = exchanger;
+            start();
+        }
+
+        @Override
+        public void run() {
+            try {
+                exchanger.exchange("My name is Mike");
+                sleep(500);
+                System.out.println(exchanger.exchange(null));
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
