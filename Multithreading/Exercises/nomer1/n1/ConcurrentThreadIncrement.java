360b8127e41608a99e3c268965d059e7b5462240
Shazon <68861895+shazon-noname@users.noreply.github.com>
2024-03-23 12:51:17 +0200

    Delete Multithreading/Exercises/nomer1/n1/ConcurrentThreadIncrement.java

--- Multithreading/Exercises/nomer1/n1/ConcurrentThreadIncrement.java
+++
@@ -1,6 +0,0 @@
-package Multithreading.Exercises.nomer1.n1;
-/*
-Write a Java program to create and start multiple threads that increment a shared counter variable concurrently.
- */
-public class ConcurrentThreadIncrement {
-}
