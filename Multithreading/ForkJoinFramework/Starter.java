15e9efa1d143baf5640d1aa83b787d454344270a
shazon <maryan.20083@gmail.com>
2024-03-24 09:33:07 +0200

    Starter.java

---
+++ Multithreading/ForkJoinFramework/Starter.java
@@ -0,0 +1,39 @@
+package Multithreading.ForkJoinFramework;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
+import java.util.concurrent.RecursiveTask;
+
+public class Starter {
+    public static void main(String[] args) {
+        final int MAX = 16;
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        forkJoinPool.invoke(new Action(MAX));
+        System.out.println("Finish");
+    }
+}
+
+ class Action extends RecursiveAction {
+    private int value;
+
+     public Action(int value) {
+         this.value = value;
+     }
+
+     @Override
+     protected void compute() {
+         if (value <= 4) {
+             System.out.println("Hello before:" + Thread.currentThread().getId() + " = " + value);
+             try {
+                 Thread.sleep(500);
+             } catch (InterruptedException e) {
+                 throw new RuntimeException(e);
+             }
+             System.out.println("Hello after: " + Thread.currentThread().getId() + " = " + value);
+         } else {
+             Action m1 = new Action(value/2);
+             Action m2 = new Action(value/2);
+             invokeAll(m1,m2);
+         }
+     }
+ }
