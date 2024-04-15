231981c07c9c04d27eb2b99eb8f7e5939a0ff0d9
Marian Zinkevych <maryan.20083@gmail.com>
2024-04-15 05:00:26 +0300

    update Multithreading package

---
+++ Multithreading/exercise/exercise12/CallableFutureExercise.java
@@ -0,0 +1,34 @@
+package Multithreading.exercise.exercise12;
+
+import java.util.concurrent.*;
+
+public class CallableFutureExercise {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Future<int[]> future = executor.submit(new FutureTask(5));
+        int[] result = future.get();
+        System.out.println("Task result: " + java.util.Arrays.toString(result));
+        executor.shutdown();
+    }
+
+
+    static class FutureTask implements Callable<int[]> {
+        private final int count;
+
+        FutureTask(int count) {
+            this.count = count;
+        }
+
+        @Override
+        public int[] call() throws Exception {
+            int[] arr = new int[count];
+            arr[0] = 0;
+            arr[1] = 1;
+            for (int i = 2; i < count; ++i) {
+                arr[i] = arr[i - 1] + arr[i - 2];
+            }
+            return arr;
+        }
+    }
+}
+
