d44a19c2b2c30ec262fedaa3af8f0d099873cadd
Marian Zinkevych <maryan.20083@gmail.com>
2024-04-16 04:43:20 +0300

    update Multithreading package

---
+++ Multithreading/PrioritiesThread/Runner.java
@@ -0,0 +1,23 @@
+package Multithreading.PrioritiesThread;
+
+import java.util.stream.IntStream;
+
+public class Runner {
+    private static final String MESSAGE_MAIN_THREAD_FINISHED = "Main thread is finished";
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(new ThreadTask());
+        thread.setPriority(10);
+        thread.start();
+        System.out.println(MESSAGE_MAIN_THREAD_FINISHED);
+    }
+
+    static class ThreadTask implements Runnable {
+        private static final int RANGE_MINIMAL_BORDER = 0;
+        private static final int RANGE_MAXIMAL_BORDER = 100;
+        @Override
+        public void run() {
+            IntStream.rangeClosed(RANGE_MINIMAL_BORDER,RANGE_MAXIMAL_BORDER).forEach(System.out::println);
+        }
+    }
+}
