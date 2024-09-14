780852c115901129fc0d96a12fe7369f7c3e3670
shazon-noname <maryan.20083@gmail.com>
2024-09-14 23:13:09 +0300

    added recursiveBinaryMethodFindElement in BinarySearch.java

--- algos/BinarySearch.java
+++ algos/BinarySearch.java
@@ -2,7 +2,7 @@ package algos;
 
 public class BinarySearch {
     static int count = 0;
-    static int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    static int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13};
 
     static int binaryMethodFindElement(int[] array, int element) {
         int start = 0;
@@ -27,8 +27,25 @@ public class BinarySearch {
         return position;
     }
 
+    public static int recursiveBinaryMethodFindElement(int[] array, int element, int start, int end) {
+        if (start > end) {
+            return -1;
+        }
+        int middle = (start + end) / 2;
+        count++;
+        if (array[middle] == element) {
+            return middle;
+        }
+        if (element < array[middle]) {
+            return recursiveBinaryMethodFindElement(array, element, start,middle-1);
+        } else {
+            return recursiveBinaryMethodFindElement(array, element, middle+1, end);
+        }
+    }
+
     public static void main(String[] args) {
-        int result = binaryMethodFindElement(array, 13);
-        System.out.println("result = " + result + "\n" + "count = " + count);
+//        int result = binaryMethodFindElement(array, 13);
+        int result2 = recursiveBinaryMethodFindElement(array, 13, 0, array.length-1);
+        System.out.println("result = " + result2 + "\n" + "count = " + count);
     }
 }
