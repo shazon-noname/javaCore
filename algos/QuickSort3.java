9427e13ce61d33cb1f502037002574e7fbf13147
shazon-noname <maryan.20083@gmail.com>
2024-09-14 22:51:33 +0300

    added QuickSort3.java

---
+++ algos/QuickSort3.java
@@ -0,0 +1,46 @@
+package algos;
+
+import java.util.Arrays;
+
+public class QuickSort3 {
+    public static void main(String[] args) {
+        int[] array = {5, 2, 3, 78, 4, 8, 32, 123};
+        System.out.println(Arrays.toString(array));
+        quickSort(array, 0, array.length - 1);
+        System.out.println(Arrays.toString(array));
+    }
+
+
+    public static void quickSort(int[] array, int low, int high) {
+        int leftMarket = low;
+        int rightMarker = high;
+        int pivot = array[(leftMarket + rightMarker) / 2];
+
+        do {
+            while (array[leftMarket] < pivot) {
+                leftMarket++;
+            }
+
+            while (array[rightMarker] > pivot) {
+                rightMarker--;
+            }
+
+            if (leftMarket <= rightMarker) {
+                if (leftMarket < rightMarker) {
+                    int temp = array[leftMarket];
+                    array[leftMarket] = array[rightMarker];
+                    array[rightMarker] = temp;
+                }
+                leftMarket++;
+                rightMarker--;
+            }
+        } while (leftMarket <= rightMarker);
+
+        if (leftMarket < high) {
+            quickSort(array, leftMarket, high);
+        }
+        if (low < rightMarker) {
+            quickSort(array, low, rightMarker);
+        }
+    }
+}
