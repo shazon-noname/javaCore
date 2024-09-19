d393c7515f42b068049c42280c7b3b0f72842b3a
shazon-noname <maryan.20083@gmail.com>
2024-09-19 20:50:52 +0300

    added methods: sumDeep, sumWide

--- algos/Tree/RecursiveTraversalInDepth.java
+++ algos/Tree/RecursiveTraversalInDepth.java
@@ -1,14 +1,16 @@
 package algos.Tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class RecursiveTraversalInDepth {
     public static void main(String[] args) {
-        Tree root = new Tree(20,
-                new Tree(7,
-                        new Tree(4, null, new Tree(6)), new Tree(9)),
-                new Tree(35,
-                        new Tree(31, new Tree(28), null),
-                        new Tree(40, new Tree(38), new Tree(52))));
-        System.out.println(root.sumValueTree());
+        Tree root = new Tree(20, new Tree(7, new Tree(4, null, new Tree(6)), new Tree(9)), new Tree(35, new Tree(31, new Tree(28), null), new Tree(40, new Tree(38), new Tree(52))));
+        System.out.println("sum recursive = " + root.recursiveSumValueTree());
+        System.out.println("sum stack = " + root.sumDeep(root));
+        System.out.println("sum queue = " + root.sumWide(root));
+
     }
 
     static class Tree {
@@ -26,13 +28,59 @@ public class RecursiveTraversalInDepth {
             this.value = value;
         }
 
-        public int sumValueTree() {
+        public int recursiveSumValueTree() {
             int sum = value;
             if (left != null) {
-                sum += left.sumValueTree();
+                sum += left.recursiveSumValueTree();
             }
             if (right != null) {
-                sum += right.sumValueTree();
+                sum += right.recursiveSumValueTree();
+            }
+            return sum;
+        }
+
+        public int sumDeep(Tree root) {
+            Stack<Tree> stack = new Stack<>();
+            stack.push(root);
+
+            int sum = 0;
+
+            while (!stack.isEmpty()) {
+                Tree node = stack.pop();
+                System.out.println(node.value); //Перегляд послідовності елементів
+                sum += node.value;
+
+                if (node.right != null) {
+                    stack.push(node.right);
+                }
+
+                if (node.left != null) {
+                    stack.push(node.left);
+                }
+            }
+            return sum;
+        }
+
+        public int sumWide(Tree root) {
+            Queue<Tree> queue = new LinkedList<>();
+            queue.add(root);
+
+            int sum = 0;
+
+            while (!queue.isEmpty()) {
+                Tree node = queue.remove();
+                System.out.println(node.value); //Перегляд послідовності елементів
+                sum += node.value;
+
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+
+
             }
             return sum;
         }
