1399b5f21d5f340fa142d155b0a18ed1c22b730e
shazon-noname <maryan.20083@gmail.com>
2024-09-20 05:39:58 +0300

    added searchFilesBFS in SearchForFilesInFoldersOnTheDisk.java

--- algos/SearchForFilesInFoldersOnTheDisk.java
+++ algos/SearchForFilesInFoldersOnTheDisk.java
@@ -2,7 +2,9 @@ package algos;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class SearchForFilesInFoldersOnTheDisk {
     public static void main(String[] args) {
@@ -13,6 +15,18 @@ public class SearchForFilesInFoldersOnTheDisk {
         for (File file1 : fileList) {
             System.out.println(file1.getAbsolutePath());
         }
+
+        fileList.clear();
+
+        System.out.println();
+
+        searchFilesBFS(file, fileList);
+
+        for (File file1 : fileList) {
+            System.out.println(file1.getAbsolutePath());
+        }
+
+
     }
 
     public static void searchFiles(File rootFile, List<File> fileList) {
@@ -23,6 +37,28 @@ public class SearchForFilesInFoldersOnTheDisk {
                 for (File file : directoryFiles) {
                     if (file.isDirectory()) {
                         searchFiles(file, fileList);
+                    } else {
+                        if (file.getName().toLowerCase().endsWith(".jpg")) {
+                            fileList.add(file);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static void searchFilesBFS(File rootFile, List<File> fileList) {
+        Queue<File> queue = new LinkedList<>();
+        queue.add(rootFile);
+
+        while (!queue.isEmpty()) {
+            File currentFile = queue.poll();
+            System.out.println("Searching at: " + rootFile.getAbsolutePath());
+            File[] directoryFiles = currentFile.listFiles();
+            if (directoryFiles!=null) {
+                for (File file : directoryFiles) {
+                    if (file.isDirectory()) {
+                        queue.add(file);
                     } else {
                         if (file.getName().toLowerCase().endsWith(".jpg")) {
                             fileList.add(file);
