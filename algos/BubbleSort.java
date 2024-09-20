aa9dd76ed6b0acf936670d8a51dd99c4d4b9f1b8
shazon-noname <maryan.20083@gmail.com>
2024-09-20 04:56:32 +0300

    added bubbleSortArray2 in BubbleSort.java

--- algos/BubbleSort.java
+++ algos/BubbleSort.java
@@ -7,17 +7,37 @@ public class BubbleSort {
     static int count = 0;
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(bubbleSortArray(array)));
+//        System.out.println(Arrays.toString(bubbleSortArray(array)));
+//        System.out.println(count);
+        System.out.println(Arrays.toString(bubbleSortArray2(array)));
+        System.out.println(count);
     }
 
 
     static int[] bubbleSortArray(int[] array) {
         for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length-1; j++) {
-                if (array[j+1] < array[j]) {
+            for (int j = 0; j < array.length - 1; j++) {
+                if (array[j + 1] < array[j]) {
                     int temp = array[j];
-                    array[j] = array[j+1];
-                    array[j+1] = temp;
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+                count++;
+            }
+        }
+        return array;
+    }
+
+    static int[] bubbleSortArray2(int[] array) {
+        boolean isSorted = false;
+        while (!isSorted) {
+            isSorted = true;
+            for (int j = 0; j < array.length - 1; j++) {
+                if (array[j + 1] < array[j]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    isSorted = true;
                 }
                 count++;
             }
