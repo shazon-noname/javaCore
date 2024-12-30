97ebb18da2b280e3d5d0af9467e16e1c3b8a6e84
shazon-noname <maryan.20083@gmail.com>
2024-12-30 05:23:51 +0200

    create new package Objects_and_Methods and 3 classes

---
+++ Objects_and_Methods/Computer.java
@@ -0,0 +1,186 @@
+package Objects_and_Methods;
+
+public class Computer {
+    String vendor;
+    String name;
+    Processor processor;
+    Ram ram;
+    Storage storage;
+    Screen  Screen ;
+    Keyboard keyboard;
+
+    public Computer(String vendor, String name, Processor processor, Ram ram, Storage storage, Objects_and_Methods.Screen screen, Keyboard keyboard) {
+        this.vendor = vendor;
+        this.name = name;
+        this.processor = processor;
+        this.ram = ram;
+        this.storage = storage;
+        Screen = screen;
+        this.keyboard = keyboard;
+        System.out.println("Initial computer configuration");
+
+    }
+
+    public Processor getProcessor() {
+        return processor;
+    }
+
+    public void setProcessor(Processor processor) {
+        this.processor = processor;
+    }
+
+    public Ram getRam() {
+        return ram;
+    }
+
+    public void setRam(Ram ram) {
+        this.ram = ram;
+    }
+
+    public Storage getStorage() {
+        return storage;
+    }
+
+    public void setStorage(Storage storage) {
+        this.storage = storage;
+    }
+
+    public Screen getScreen() {
+        return Screen;
+    }
+
+    public void setScreen(Screen screen) {
+        Screen = screen;
+    }
+
+    public Keyboard getKeyboard() {
+        return keyboard;
+    }
+
+    public void setKeyboard(Keyboard keyboard) {
+        this.keyboard = keyboard;
+    }
+
+    @Override
+    public String toString() {
+        return "Computer{" +
+                "processor=" + processor +
+                ", ram=" + ram +
+                ", storage=" + storage +
+                ", Screen=" + Screen +
+                ", keyboard=" + keyboard +
+                '}';
+    }
+}
+
+
+
+final class  Processor {
+    private double frequency;
+    private int threads;
+    private String manufacturer;
+    private double weight;
+
+    Processor(double frequency, int threads, String manufacturer, double weight) {
+        this.frequency = frequency;
+        this.threads = threads;
+        this.manufacturer = manufacturer;
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Processor{" +
+                "frequency=" + frequency +
+                ", threads=" + threads +
+                ", manufacturer='" + manufacturer + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
+}
+
+final class Ram {
+    private String type;
+    private int size;
+    private double weight;
+
+    public Ram(String type, int size, double weight) {
+        this.type = type;
+        this.size = size;
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Ram{" +
+                "type='" + type + '\'' +
+                ", size=" + size +
+                ", weight=" + weight +
+                '}';
+    }
+}
+
+final class Storage {
+    private String type;
+    private int capacity;
+    private double weight;
+
+    public Storage(String type, int capacity, double weight) {
+        this.type = type;
+        this.capacity = capacity;
+        this.weight = weight;
+
+
+    }
+
+    @Override
+    public String toString() {
+        return "Storage{" +
+                "type='" + type + '\'' +
+                ", capacity=" + capacity +
+                ", weight=" + weight +
+                '}';
+    }
+}
+
+final class Screen  {
+    private double diagonal;
+    private String type;
+    private double weight;
+
+    public Screen(double diagonal, String type, double weight) {
+        this.diagonal = diagonal;
+        this.type = type;
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Screen{" +
+                "diagonal=" + diagonal +
+                ", type='" + type + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
+}
+
+final class Keyboard {
+    private String type;
+    private boolean backlit;
+    private double weight;
+
+    public Keyboard(String type, boolean backlit, double weight) {
+        this.type = type;
+        this.backlit = backlit;
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Keyboard{" +
+                "type='" + type + '\'' +
+                ", backlit=" + backlit +
+                ", weight=" + weight +
+                '}';
+    }
+}
---
+++ Objects_and_Methods/Main.java
@@ -0,0 +1,30 @@
+package Objects_and_Methods;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Basket basket = new Basket();
+        basket.add("Milk", 40);
+        basket.print("Milk");
+
+        System.out.println();
+
+        Processor processor = new Processor(4.25, 12, "AMD", 0.6 );
+        Ram ram = new Ram("DDR4", 16, 0.1);
+        Storage storage = new Storage("SSD", 468, 0.2);
+        Screen screen = new Screen(24, "TN", 5);
+        Keyboard keyboard = new Keyboard("mechanical",true, 1);
+
+        Computer computer = new Computer("Dell", "Inspiron 15",processor,ram, storage, screen, keyboard);
+        System.out.println(computer);
+
+        Processor newProcessor = new Processor(4.2, 8, "AMD", 0.35);
+        computer.setProcessor(newProcessor);
+
+        Ram newRam = new Ram("DDR5", 32, 0.12);
+        computer.setRam(newRam);
+
+        System.out.println("\nAfter upgrading components:");
+        System.out.println(computer);
+    }
+}
