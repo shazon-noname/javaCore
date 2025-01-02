b8be663a5ade9887e685d6a551b7c41b9deca915
shazon-noname <maryan.20083@gmail.com>
2025-01-02 06:09:14 +0200

    added dataBirth() in Birthdays.java

--- NumberAndDateExperiments/practice/Birthdays.java
+++ NumberAndDateExperiments/practice/Birthdays.java
@@ -2,10 +2,13 @@ package NumberAndDateExperiments.practice;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Locale;
 
 public class Birthdays {
     public static void main(String[] args) {
-        System.out.println(collectBirthdays(2002,2,24));
+//        System.out.println(collectBirthdays(2002,2,24));
+        System.out.println(dataBirth(2002,2,24));
     }
     public static String collectBirthdays(int year, int month, int day) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
@@ -25,5 +28,29 @@ public class Birthdays {
         }
 
         return result.toString();
+    }
+
+
+    public static String dataBirth(int year, int month, int day) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL)
+                .localizedBy(Locale.of("uk"));
+
+        LocalDate birthday = LocalDate.of(year, month, day);
+        LocalDate localDate= LocalDate.now();
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        int count = 0;
+
+        while (!birthday.isAfter(localDate)) {
+            stringBuilder.append(count++)
+                    .append(" - ")
+                    .append(birthday.format(dateTimeFormatter))
+                    .append(" - ")
+                    .append(System.lineSeparator());
+            birthday = birthday.plusYears(1);
+        }
+
+        return stringBuilder.toString();
     }
 }
