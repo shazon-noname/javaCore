d4cf48b7c94b587989f3baed751ffab9385135df
shazon-noname <maryan.20083@gmail.com>
2025-01-27 00:28:51 +0200

    removed all calculate classes and used anonymous classes

--- functionalInterface/practice2/ActionSum.java
+++
@@ -1,9 +0,0 @@
-package functionalInterface.practice2;
-
-public class ActionSum implements Action<Integer>{
-
-    @Override
-    public Integer calculate(Integer t1, Integer t2) {
-        return t1 + t2;
-    }
-}
--- functionalInterface/practice2/Calculator.java
+++ functionalInterface/practice2/Calculator.java
@@ -5,22 +5,42 @@ public class Calculator {
         int a = 5;
         int b = 3;
 
-        Action<Integer> actionSum = new ActionSum();
+        Action<Integer> actionSum = new Action<Integer>() {
+            @Override
+            public Integer calculate(Integer t1, Integer t2) {
+                return t1 + t2;
+            }
+        };
         computation(a, b,actionSum);
 
-        Action<Integer> subtract = new ActionSubtract();
+        Action<Integer> subtract = new Action<Integer>() {
+            @Override
+            public Integer calculate(Integer t1, Integer t2) {
+                return t1 - t2;
+            }
+        };
         computation(a,b,subtract);
 
-        Action<Integer> divide = new ActionDivide();
+        Action<Integer> divide = new Action<Integer>() {
+            @Override
+            public Integer calculate(Integer t1, Integer t2) {
+                return t1 / t2;
+            }
+        };
         computation(a,b,divide);
 
-        Action<Integer> multiply = new ActionMultiply();
+        Action<Integer> multiply = new Action<Integer>() {
+            @Override
+            public Integer calculate(Integer t1, Integer t2) {
+                return t1 * t2;
+            }
+        };
         computation(a,b,multiply);
     }
 
     private static void computation(int a, int b,Action<Integer> action) {
         int result = calculate(a, b,action);
-        System.out.println(action.getClass().getSimpleName() + " = " + result);
+        System.out.println( action.getClass() +  " = " + result);
     }
 
     private static int calculate (int a, int b, Action<Integer> action) {
