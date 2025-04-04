7f6a02fd6a9f8886aaa3be617f9729947aee4de8
shazon-noname <maryan.20083@gmail.com>
2025-04-04 21:57:33 +0300

    Add first example JavaNIO

---
+++ Working_with_files_and_networking/JavaNIO/Example.java
@@ -0,0 +1,34 @@
+package Working_with_files_and_networking.JavaNIO;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+
+public class Example {
+    public static void main(String[] args) {
+        ByteBuffer buff = ByteBuffer.allocate(20);
+
+        // Запис у буфер
+        buff.put((byte) 'H'); // position = 1, одиночний запис
+        // буфер: ['H', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        buff.put("ello world".getBytes()); // position = 11, запис масиву байт
+        // буфер: ['H','e','l','l','o',' ','w','o','r','l','d',0,0,0,0,0,0,0,0,0]
+        buff.put(6, "Ukrainian".getBytes()); // запис з індексу 6, position не змінюється
+        // буфер: ['H','e','l','l','o',' ','U','k','r','a','i','n','i','a','n',0,0,0,0,0]
+
+        // Читання з буфера
+        buff.flip(); // position = 0, limit = 15 (останній записаний байт + 1)
+        char h = (char) buff.get(); // position = 1, зчитує 'H'
+        char e = (char) buff.get(); // position = 2, зчитує 'e'
+        char u = (char) buff.get(6); // зчитує 'U' з індексу 6, position залишається 2
+        char k = (char) buff.get(7); // зчитує 'k' з індексу 7, position залишається 2
+
+        byte[] hello = new byte[5];
+        buff.get(0, hello); // копіює 5 байт з індексу 0 у масив hello
+        String result = new String(hello, StandardCharsets.UTF_8);
+        System.out.println(result); // Виведе "Hello"
+
+        char x = (char) buff.get(14); // Спроба зчитати останній символ 'n' за межами limit = 11
+        System.out.println("Останній символ: " + x);
+
+    }
+}
