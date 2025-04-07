f82b5eefba214444f71650a92b0c5dff79f2898a
shazon-noname <maryan.20083@gmail.com>
2025-04-07 06:32:06 +0300

    Add DemoReadWrite.java and refactor CopyExampleMp.java ( use try with)

--- Working_with_files_and_networking/CopyExampleMp.java
+++ Working_with_files_and_networking/CopyExampleMp.java
@@ -1,22 +1,21 @@
 package Working_with_files_and_networking;
 
 import java.io.FileInputStream;
-
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class CopyExampleMp {
-    public static void main() throws IOException {
-        FileInputStream is = new FileInputStream("working_with_files_and_networking/cats.mp4");
-        FileOutputStream os = new FileOutputStream("working_with_files_and_networking/cats2.mp4");
-
-        byte[] buffer = new byte[4096];
-        int read = is.read(buffer);
-        while (read != -1) {
-            os.write(buffer,0,read);
-            read = is.read(buffer);
+    public static void main() {
+        try (FileInputStream is = new FileInputStream("working_with_files_and_networking/cats.mp4");
+             FileOutputStream os = new FileOutputStream("working_with_files_and_networking/cats2.mp4")) {
+            byte[] buffer = new byte[4096];
+            int read = is.read(buffer);
+            while (read != -1) {
+                os.write(buffer, 0, read);
+                read = is.read(buffer);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        is.close();
-        os.close();
     }
 }
---
+++ Working_with_files_and_networking/DemoReadWrite.java
@@ -0,0 +1,21 @@
+package Working_with_files_and_networking;
+
+import java.io.*;
+
+public class DemoReadWrite {
+    public static void main() {
+        try (Reader reader = new FileReader("log4j2.xml");
+             Writer writer = new FileWriter("Working_with_files_and_networking/testLog.xml")) {
+            char[] buffer = new char[4096];
+            int r = reader.read(buffer);
+            while (r != -1) {
+                writer.write(buffer, 0, r);
+                r = reader.read(buffer);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
